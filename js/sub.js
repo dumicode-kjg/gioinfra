62d371d5b040f3638cfee7e6e8b85d087e44cb9c
dumicode <dumicode@naver.com>
2024-08-04 14:09:15 +0900

    탭영역 작업중

--- js/sub.js
+++ js/sub.js
@@ -22,4 +22,39 @@ $(function () {
   $(document).click(function () {
     $(".sub_depth_box > a").removeClass("active");
   });
+
+  //서브탭(모바일)
+  $(".sub_tabbox a").on("click", function () {
+    if ($(this).parent("li").hasClass("active")) {
+      $(".sub_tabbox ul").not($(this).parents("ul")).removeClass("open");
+      $(this).parents("ul").toggleClass("open");
+      return false;
+    }
+  });
+  //서브탭
+  $(".js_tab .sub_tabbox a").on("click", function () {
+    if (!$(this).parent("li").hasClass("active")) {
+      $(this).parents("ul").removeClass("open");
+    }
+
+    $(this).parents(".sub_tabbox").find("a").removeAttr("title");
+    $(this).attr("title", "현재 활성화");
+    $(this).parents(".sub_tabbox").find("li").removeClass("active");
+    $(this).parent("li").addClass("active");
+    $(this).parents(".js_tab").find(".js_tabcont").removeClass("active");
+    $(this).parents(".js_tab").find(".js_tabcont").eq($(".js_tab a").index(this)).addClass("active");
+
+    return false;
+  });
+  $(document).click(function () {
+    $(".sub_tabbox ul").removeClass("open");
+  });
+
+  //모바일버전 table, grid 스크롤
+  $(".mobile_scroll").on("scroll", function () {
+    $(this).find(".txt_ex_scroll").remove();
+  });
+  $(".mobile_scroll .txt_ex_scroll").on("click", function () {
+    $(this).remove();
+  });
 });
